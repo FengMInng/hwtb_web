e4a11bc558a1fed0772822d0e2f00188d227ca2b
mfeng <mfeng@gmail.com>
2016-01-19 23:06:47 +0800

    run ok

--- hwtb/urls.py
+++ hwtb/urls.py
@@ -18,7 +18,6 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^index/', 'index.html'),
     url(r'^admin/', admin.site.urls),
     url(r'^polls/', include('polls.urls')),
 
--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 # Create your views here.
@@ -16,11 +17,17 @@ class IndexView(generic.ListView):
 	
 	def get_queryset(self):
 		"""Return the last five publish question."""
-		return Question.objects.order_by('-pub_date')[:5]
+		return Question.objects.filter(
+        	pub_date__lte=timezone.now()
+    	 ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
 	model = Question
 	template_name = 'polls/detail.html'
+	
+	def get_queryset(self):
+		return Question.objects.filter(pub_date__lte=timezone.now())
+
 	
 class ResultsView(generic.DetailView):
 	model = Question
