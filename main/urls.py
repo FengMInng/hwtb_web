fad8d0412111ef56d205a84c2a3be912bb1d164d
mfeng <mfeng@gmail.com>
2016-01-20 23:07:29 +0800

    redirect to jd

---
+++ main/urls.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*- 
+'''
+Created on 2016年1月19日
+
+@author: fangfang
+'''
+from django.conf.urls import url
+
+from . import views
+
+app_name='main'
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
