e6395b5006115c7266ac5289cb51b27f9cab2974
unknown <ming2fang@gmail.com>
2016-01-24 17:32:19 +0800

    add some static file

--- main/models.py
+++ main/models.py
@@ -3,3 +3,38 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class ProductCatalog(models.Model):
+    '''
+    this model is for product catalog, which catalog have some  product
+    '''
+    # product catalog name
+    name = models.CharField(max_length=100)
+    # product descriptor
+    descriptor = models.CharField(max_length=500)
+    # create date and time
+    create_date = models.DateTimeField()
+    #create user
+    create_user = models.CharField(max_length=100)
+    
+    #validate date
+    show_from = models.DateField()
+    show_end = models.DateField()
+    
+    #if delete, remain
+    is_delete = models.BooleanField(False)
+    
+    def __unicode__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    '''
+    this model is for product
+    '''
+    #parent catalog
+    catalog = models.ForeignKey(ProductCatalog,on_delete=models.CASCADE)
+    
+    #show name
+    name = models.CharField(max_length=100)
+    descriptor = models.CharField(max_length=500)
--- main/views.py
+++ main/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html', {})
+    web_content={'pc_style_color':1}
+    return render(request, 'main/index.html', web_content)
